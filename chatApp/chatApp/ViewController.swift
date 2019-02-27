0c1db6d702d3f76b65c6ab755c3236cf8f25d20c
yamamoto tatsuya <52.wadachi.96@gmail.com>
2019-02-27 20:26:49 +0900

    init

---
+++ chatApp/chatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatApp
+//
+//  Created by 山本竜也 on 2019/2/27.
+//  Copyright © 2019 山本竜也. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
